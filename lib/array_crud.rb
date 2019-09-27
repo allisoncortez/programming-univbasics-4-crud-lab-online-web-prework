43f938c5733902168b92d2c5556212c923fbdf99
allison cortez <allizetorc@gmail.com>
2019-09-27 22:18:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,6 @@ end
 def add_element_to_end_of_array(array, element)
  array = [element1, element2]
   array << element
-  p add_element_to_end_of_array  
 end
 
 def add_element_to_start_of_array(array, element)
